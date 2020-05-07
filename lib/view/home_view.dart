e054edea8c278f8ba5e3064697b9c35ce81ac5a7
linnefromice <linnefromice@gmail.com>
2020-05-07 20:37:54 +0900

    create NavigationBar

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_website/widgets/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
   HomeView({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: NavigationBar(),
+    );
   }
 }
