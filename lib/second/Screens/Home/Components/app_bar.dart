b02c72e537aa6e0c8e7585dfb33a83aab2d546e4
linnefromice <linnefromice@gmail.com>
2020-05-22 00:04:43 +0900

    add menu item to app bar (no function)

--- lib/second/Screens/Home/Components/app_bar.dart
+++ lib/second/Screens/Home/Components/app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_website/second/constant.dart';
 
 class CustomAppBar extends StatelessWidget {
   @override
@@ -19,13 +20,65 @@ class CustomAppBar extends StatelessWidget {
       ),
       child: Row(
         children: <Widget>[
-          Image.asset("assets/second/images/logo.png"),
+          Image.asset(
+            "assets/second/images/logo.png",
+            height: 25,
+            alignment: Alignment.topCenter,
+          ),
           SizedBox(width: 5),
           Text(
             "Foodi".toUpperCase(),
             style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          )
+          ),
+          Spacer(),
+          MenuItem(
+            title: "Home",
+            press: () {},
+          ),
+          MenuItem(
+            title: "about",
+            press: () {},
+          ),
+          MenuItem(
+            title: "pricing",
+            press: () {},
+          ),
+          MenuItem(
+            title: "contact",
+            press: () {},
+          ),
+          MenuItem(
+            title: "login",
+            press: () {},
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class MenuItem extends StatelessWidget {
+  final String title;
+  final Function press;
+  const MenuItem({
+    Key key,
+    this.title,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: press,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Text(
+          title.toUpperCase(),
+          style: TextStyle(
+            color: kTextColor.withOpacity(0.3),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
