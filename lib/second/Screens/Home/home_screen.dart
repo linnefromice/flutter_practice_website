25780dc1f80c65c45bf36bf1f0c919ea4d9125a5
linnefromice <linnefromice@gmail.com>
2020-05-21 23:39:45 +0900

    add background & change assets (add base assets in reference)

--- lib/second/Screens/Home/home_screen.dart
+++ lib/second/Screens/Home/home_screen.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Container(
+      height: size.height,
+      width: size.width,
+      // it will take full width
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/second/images/bg.png"),
+          fit: BoxFit.cover
+        )
+      ),
+    );
   }
 }
