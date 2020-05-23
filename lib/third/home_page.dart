f65aa15cf7689ed1292b47b39686cc336d314e4f
linnefromice <linnefromice@gmail.com>
2020-05-23 18:13:28 +0900

    update third - add body (add only background)

--- lib/third/home_page.dart
+++ lib/third/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_website/third/utils/responsive_layout.dart';
 import 'package:flutter_practice_website/third/widgets/navbar.dart';
 
 class HomePage extends StatelessWidget {
@@ -18,10 +19,39 @@ class HomePage extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              NavBar()
+              NavBar(),
+              Body(),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveLayout(
+      largeScreen: LargeChild(),
+    );
+  }
+}
+
+class LargeChild extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 600,
+      child: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          FractionallySizedBox(
+            alignment: Alignment.centerRight,
+            widthFactor: 0.6,
+            child: Image.network("assets/third/image_01.png", scale: 0.85),
+          )
+        ],
       ),
     );
   }
