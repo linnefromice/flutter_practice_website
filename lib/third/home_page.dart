24758c2e3d2bf3bab0907c72e19066b492b7f434
linnefromice <linnefromice@gmail.com>
2020-05-24 15:37:59 +0900

    separate search part

--- lib/third/home_page.dart
+++ lib/third/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_website/third/utils/responsive_layout.dart';
 import 'package:flutter_practice_website/third/widgets/navbar.dart';
+import 'package:flutter_practice_website/third/widgets/search.dart';
 import 'package:flutter_practice_website/third/widgets/sendbtn.dart';
 
 class HomePage extends StatelessWidget {
@@ -96,47 +97,7 @@ class LargeChild extends StatelessWidget {
                   SizedBox(
                     height: 40,
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: 4.0,
-                      right: ResponsiveLayout.isSmallScreen(context) ? 4 : 74,
-                      top: 10,
-                      bottom: 40,
-                    ),
-                    child: Container(
-                      height: 60,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                        boxShadow: [BoxShadow(
-                          color: Colors.black12,
-                          offset: Offset(0, 8),
-                          blurRadius: 8,
-                        )],
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Expanded(
-                              flex: 8,
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Your Email Address'
-                                ),
-                              ),
-                            ),
-                            Expanded(
-                              flex: 2,
-                              child: SendBtn(),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  )
+                  Search()
                 ],
               ),
             ),
