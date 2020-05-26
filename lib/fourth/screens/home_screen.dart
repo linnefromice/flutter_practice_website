f6c53068bde2b80c73bc2f3cffa17e56c830b6aa
linnefromice <linnefromice@gmail.com>
2020-05-26 13:59:04 +0900

    for fourth - create GestureDetector for launching to article's url

--- lib/fourth/screens/home_screen.dart
+++ lib/fourth/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_practice_website/fourth/models/article_model.dart';
 import 'package:flutter_practice_website/fourth/services/api_service.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -40,55 +41,66 @@ class _State extends State<HomeScreen> {
     );
   }
 
+  _launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   _buildArticleTile(Article article, MediaQueryData mediaQuery) {
     return GridTile(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 250.0,
-            alignment: Alignment.bottomCenter,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0),
-              ),
-              image: DecorationImage(
-                image: NetworkImage(article.imageUrl),
-                fit: BoxFit.cover
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            alignment: Alignment.center,
-            height: 120.0,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20.0),
-                bottomRight: Radius.circular(20.0),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  offset: Offset(0, 1),
-                  blurRadius: 6.0
+      child: GestureDetector(
+        onTap: () => _launchUrl(article.url),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 250.0,
+              alignment: Alignment.bottomCenter,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
                 ),
-              ],
-            ),
-            child: Text(
-              article.title,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
+                image: DecorationImage(
+                  image: NetworkImage(article.imageUrl),
+                  fit: BoxFit.cover
+                ),
               ),
-              maxLines: 2,
             ),
-          )
-        ],
+            Container(
+              padding: EdgeInsets.all(10.0),
+              alignment: Alignment.center,
+              height: 120.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20.0),
+                  bottomRight: Radius.circular(20.0),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 1),
+                    blurRadius: 6.0
+                  ),
+                ],
+              ),
+              child: Text(
+                article.title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                maxLines: 2,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
