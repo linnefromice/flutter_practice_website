924fcdfc1c10c2292f499c6477314a542073a4d0
linnefromice <linnefromice@gmail.com>
2020-06-01 14:51:30 +0900

    update fifth - add logo

--- lib/fifth/home.dart
+++ lib/fifth/home.dart
@@ -4,6 +4,8 @@ import 'package:flutter_practice_website/fifth/utils/bg_painter.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final bool isTablet = size.width > 1000;
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
@@ -11,8 +13,27 @@ class Home extends StatelessWidget {
           CustomPaint(
             painter: BackgroundPainter(),
           ),
+          ListView(
+            children: <Widget>[
+              _appBar(size, isTablet)
+            ],
+          )
         ],
       ),
     );
   }
+
+  _appBar(Size size, bool isTablet) => Container(
+    height: 85,
+    padding: EdgeInsets.only(right: 80),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: 50, top: 20),
+          child: Image.asset("assets/fifth/Logo.png"),
+        )
+      ],
+    ),
+  );
 }
